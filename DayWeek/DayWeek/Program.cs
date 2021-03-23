37fa8b1ff2eef52e93bf8f42b74ef7a1ea099360
unknown <voroncovleonid.45@mail.ru>
2021-03-24 00:11:38 +0500

    commit

---
+++ DayWeek/DayWeek/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+
+namespace DayWeek
+{
+    public class DaysWeek : IEnumerator
+    {
+        public static string [] arrayDays =  { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+        int index = -1;
+
+        string a;
+
+        public bool MoveNext()
+        {
+            index ++;
+            return (index < arrayDays.Length);
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+        public object Current
+        {
+            get
+            {
+                a = arrayDays[index];
+                return a;
+            }
+        }
+    }
+
+
+    public class TransferDaysArray : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return DaysWeek.arrayDays.GetEnumerator();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TransferDaysArray dayWeek = new TransferDaysArray();
+            foreach(var arrayDays in dayWeek)
+            {
+                Console.WriteLine($"День недели: {arrayDays}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
